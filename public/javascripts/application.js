9a36b2698a7b1501f3213caedee687436bbb2e31
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2011-01-31 13:40:18 +0300

    Donate button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,34 +1,44 @@
+(function ($) {
+    $.fn.fadeOnHover = function (start, step) {
+        start = start || .5;
+        step = step || 0.07;
+
+        var timer,
+            limit = start,
+            $f = $(this);
+
+
+        if ($.browser.msie) {
+            $f.css('filter', 'alpha(opacity=50)');
+        } else {
+            $f.css('opacity', start);
+        }
+
+        $f.hover(function () {
+            if (timer) clearInterval(timer);
+            timer = setInterval(function () {
+                if ($.browser.msie) {
+                    $f.css('filter', 'alpha(opacity=' + (start += step) * 100 + ')');
+                } else {
+                    $f.css('opacity', start += step);
+                }
+                if (start + 0.001 > 1) clearInterval(timer);
+            }, 50);
+        }, function () {
+            if (timer) clearInterval(timer);
+            timer = setInterval(function () {
+                if ($.browser.msie) {
+                    $f.css('filter', 'alpha(opacity=' + (start -= step) * 100 + ')');
+                } else {
+                    $f.css('opacity', start -= step);
+                }
+                if (start - 0.0001 < limit) clearInterval(timer);
+            }, 50);
+        });
+    };
+})(jQuery);
+
 $(function () {
-    var timer,
-        start = 0.5,
-        step = 0.07,
-        $f = $('.footer');
-
-    if ($.browser.msie) {
-        $f.css('filter', 'alpha(opacity=50)');
-    } else {
-        $f.css('opacity', .5);
-    }
-
-    $f.hover(function () {
-        if (timer) clearInterval(timer);
-        timer = setInterval(function () {
-            if ($.browser.msie) {
-                $f.css('filter', 'alpha(opacity=' + (start += step) * 100 + ')');
-            } else {
-                $f.css('opacity', start += step);
-            }
-            if (start + 0.001 > 1) clearInterval(timer);
-        }, 50);
-    }, function () {
-        if (timer) clearInterval(timer);
-        timer = setInterval(function () {
-            if ($.browser.msie) {
-                $f.css('filter', 'alpha(opacity=' + (start -= step) * 100 + ')');
-            } else {
-                $f.css('opacity', start -= step);
-            }
-            if (start - 0.001 < .5) clearInterval(timer);
-        }, 50);
-    });
+    $('.footer').fadeOnHover();
+    $('.donate').fadeOnHover(.3);
 });
